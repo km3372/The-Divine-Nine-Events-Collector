ffa2d6cc10ebeb0db592e984dd8e37ffdf3eccaf
Ken Moody <km3372@gmail.com>
2017-03-06 00:08:09 -0500

    Sec Push add imageView

---
+++ The Divine Nine Events Collector/theDivineNineEventsViewController.swift
@@ -0,0 +1,28 @@
+//
+//  theDivineNineEventsViewController.swift
+//  The Divine Nine Events Collector
+//
+//  Created by Ken Moody on 3/5/17.
+//  Copyright © 2017 Ken Moody. All rights reserved.
+//
+
+import UIKit
+
+class theDivineNineEventsViewController: UIViewController {
+    @IBOutlet weak var eventsimageView: UIImageView!
+    @IBOutlet weak var titleTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func photosTapped(_ sender: Any) {
+    }
+
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+}
